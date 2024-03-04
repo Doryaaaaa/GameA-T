07388469eaee60355432d55d531dbd7f352de4f9
maguroyukke <maguroyukke114@gmail.com>
2024-03-04 23:24:11 +0900

    a

---
+++ Training/ASAYAMA/Sample/Sample/Project/GameProject/Slime.cpp
@@ -0,0 +1,57 @@
+#include"Slime.h"
+//コンストラクタ
+Slime::Slime(int type, const CVector2D& pos)
+   :m_pos(pos)
+   ,m_animData(nullptr)
+   ,m_img(nullptr)
+   ,m_hp(100)
+   ,m_type(type)
+{
+	int frame = 6;
+	m_animData = new TexAnimData[1]
+	{
+		{
+		new TexAnim[4]
+		{
+			{0,frame},{1,frame},
+		    {2,frame},{3,frame},
+			},
+			4
+	},
+	};
+	std::string imagePath;
+	if (m_type == 0)imagePath = "slime_a.png";
+	else if (m_type == 1)imagePath = "slime_b.png";
+	else if (m_type == 2)imagePath = "slime_c.png";
+		m_img = CImage::CreateImage
+	(imagePath.c_str(),
+		m_animData,
+		256.0f, 256.0f);
+	m_img->ChangeAnimation(0);
+	m_img->SetCenter(128.0f, 184.0f);
+	m_img->SetFlipH(true);
+}
+//デストラクタ
+Slime::~Slime() {
+	for (int i = 0; i < 2; i++) {
+		delete[]m_animData[i].pData;
+	}
+	delete[]m_animData;
+	delete m_img;
+}
+//プレイヤー座標取得
+const CVector2D& Slime::GetPos()const {
+	return m_pos;
+}
+void Slime::SetPos(const CVector2D& pos) {
+	m_pos = pos;
+}
+//更新処理
+void Slime::Update() {
+	m_img->SetPos(m_pos);
+	m_img->UpdateAnimation();
+}
+//描画処理
+void Slime::Render() {
+	m_img->Draw();
+}
--- Training/ASAYAMA/Sample/Sample/Project/GameProject/player.h
+++ Training/ASAYAMA/Sample/Sample/Project/GameProject/player.h
@@ -1,1 +1,35 @@
 #pragma once
+
+class Player {
+	//自分以外の外部からアクセスし放題
+public:
+	//コンストラクタ
+	Player();
+	//デストラクタ
+~Player();
+	/// <summary>
+	/// プレイヤーの座標を取得
+	/// </summary>
+	/// <returns>プレイヤーの座標</returns>
+const CVector2D& GetPos() const;
+	/// <summary>
+	/// プレイヤーの座標を設定
+	/// </summary>
+	/// <param name="pos">設定する座標</param>
+	void SetPos(const CVector2D& pos);
+	//更新処理
+	void Update();
+	//描画処理
+	void Render();
+
+	//自分自身のクラスしかアクセスできない
+private:
+	CVector2D m_pos;//プレイヤーの座標
+	TexAnimData* m_animData;
+	CImage* m_img;//プレイヤーの座標
+	int m_hp;//プレイヤーの体力
+
+//自分と自クラス継承したクラスでアクセスできる
+protected:
+
+};
