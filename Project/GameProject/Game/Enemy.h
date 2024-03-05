ec5b8f31a0df600c6553ad9645b5a1cd82aacbfd
maguroyukke <maguroyukke114@gmail.com>
2024-03-05 16:38:14 +0900

    a

--- Project/GameProject/Game/Enemy.h
+++ Project/GameProject/Game/Enemy.h
@@ -1,1 +1,14 @@
 #pragma once
+#include"../Base/Base.h"
+
+class Enemy :public Base{
+public:
+	Enemy();
+
+	void Update();
+	void Draw();
+private:
+	CImage m_img;
+	int m_hp;
+};
+extern TexAnimData enemy_anim_data[];
