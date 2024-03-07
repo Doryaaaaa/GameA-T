99228d95e19284a7148f80818b7cf98bed09e0d8
maguroyukke <maguroyukke114@gmail.com>
2024-03-07 10:29:16 +0900

    a

--- Project/GameProject/Game/Enemy.cpp
+++ Project/GameProject/Game/Enemy.cpp
@@ -9,6 +9,7 @@ TexAnimData enemy_anim_data[] = {
 	{stand_by_anim,sizeof(stand_by_anim) / sizeof(stand_by_anim[0])}
 };
 Enemy::Enemy(const CVector3D&pos):Base(eType_Enemy) {
+	m_pos = pos;
 	m_img = COPY_RESOURCE("Enemy", CImage);
 	
 	m_img.ChangeAnimation(0);
@@ -19,10 +20,10 @@ Enemy::Enemy(const CVector3D&pos):Base(eType_Enemy) {
 void Enemy::Update() {
 	const int move_speed = 4;
 	if (HOLD(CInput::eUp)) {
-		m_pos.z += move_speed;
+		m_pos.y += move_speed;
 	}
 	if (HOLD(CInput::eDown)) {
-		m_pos.z -= move_speed;
+		m_pos.y -= move_speed;
 	}
 	m_img.UpdateAnimation();
 }
