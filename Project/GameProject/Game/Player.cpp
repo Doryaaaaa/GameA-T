8175c3e997377cc273d61d39003b1357348fcd4a
kikkeru <23051910@d.anabuki-college.net>
2024-03-08 12:59:18 +0900

    a

--- Project/GameProject/Game/Player.cpp
+++ Project/GameProject/Game/Player.cpp
@@ -11,7 +11,7 @@ Player::Player(const CVector3D& pos) :Base(eType_Player) {
     //中心位置設定
     m_img.SetCenter(100, 100);
     //当たり判定
-    m_rect = CRect(-50, -50, 50, 50);
+    m_rect = CRect(50, 50, 50, 50);
     //通常状態(常に走り)へ
     m_state = eState_Run;
     //着地フラグ
@@ -71,7 +71,7 @@ void Player::Update() {
 void Player::Draw() {
     m_img.SetPos(GetScreenPos(m_pos));
     m_img.Draw();
-    //DrawRect();
+    DrawRect();
 }
 
 void Player::Collision(Base* b)
@@ -118,10 +118,10 @@ void Player::StateRun() {
     }
 */
     //ジャンプ(スペース)
-    /*if (m_is_ground && PUSH(CInput::eButton5)) {
+    if (m_is_ground && PUSH(CInput::eButton5)) {
         m_vec.y = -jump_pow;
         m_is_ground = false;
-    }*/
+    }
 
     m_img.ChangeAnimation(0);
 }
