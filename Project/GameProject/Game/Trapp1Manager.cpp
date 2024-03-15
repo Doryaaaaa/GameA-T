3d5f0b99471014d2522a1350fdbf0a6288b3f237
maguroyukke <maguroyukke114@gmail.com>
2024-03-15 15:34:14 +0900

    a

--- Project/GameProject/Game/Trapp1Manager.cpp
+++ Project/GameProject/Game/Trapp1Manager.cpp
@@ -1,5 +1,7 @@
 #include "Trapp1Manager.h"
 #include "Trapp1.h"
+#include "Trapp2.h"
+
 Trapp1Manager::Trapp1Manager() : ObjectBase(eType_Trapp1Manager)
 {
 	//肉出現までの時間
@@ -11,8 +13,18 @@ void Trapp1Manager::Update()
 	m_cnt--;
 	//肉出現
 	if (m_cnt <= 0) {
+		switch (rand()%2)
+		{
+		case 0:
+			(new Trapp1(CVector3D(m_scroll.x + Utility::Rand(2000.0f, 2500.0f), 900, -390)));
+			break;
+		case 1:
+			(new Trapp2(CVector3D(m_scroll.x + Utility::Rand(2000.0f, 2500.0f), 900, -390)));
+			break;
+		}
 		//X軸は12～1800の間でランダム
-		(new Trapp1(CVector3D(m_scroll.x+Utility::Rand(2000.0f, 2500.0f),900,-390)));
+		
+
 		//次の出現時間
 		m_cnt = Utility::Rand(40, 120);
 	}
